d942ae8aea04c7217e95295ea8323e1f936551b9
Chinmaya Kumar Biswal <chinmayakumarbiswal16045@gmail.com>
2023-04-02 01:07:29 +0530

    fetch API to get data

--- src/components/Contents/Contents.jsx
+++ src/components/Contents/Contents.jsx
@@ -1,8 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from "react"
+import './Contents.css'
 
 const Contents = () => {
+
+    const [allvideos, setVideos] = useState([])
+
+    const fetchAPIData = () => {
+        fetch("https://my-json-server.typicode.com/horizon-code-academy/fake-movies-api/db")
+        .then(response => {
+            return response.json()
+        })
+        .then(data => {
+            setVideos(data.movies)
+            console.log(data.movies)
+        })
+    }
+
+    useEffect(() => {
+        fetchAPIData()
+    }, [])  
   return (
-    <div>Contents</div>
+    <div className="content">
+        {allvideos.length > 0 && (
+            <div className="videos">
+                
+                {allvideos.map(videosdata => (
+                    <div className="video">
+                        <a href="https://chinmayakumarbiswal.in/">
+                            <div className="thumbnail">
+                                <img src={videosdata.Poster} alt="" />
+                            </div>
+                        </a>
+
+                        <div className="details">
+                            <div className="author">
+                                <img src={videosdata.Poster} alt="" />
+                            </div>
+                            <div className="title">
+                                <h3>{videosdata.Title}</h3>
+                                <span>Year of Post {videosdata.Year} </span>
+                            </div>
+                        </div>
+
+                    </div>
+                ))}
+
+            </div>
+        )}
+    </div>
+    
   )
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import './Header.css'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+        <div className="logo left">
+            <i id="menu" className="material-icons">menu</i>
+            <img src="yt-logo.png" alt="youtube"/>
+        </div>
+            
+        <div className="search center">
+            <form action="">
+                <input type="text" placeholder="Search" />
+                <button><i className="material-icons">search</i></button>
+            </form>
+            <i className="material-icons mic">mic</i>
+        </div>
+            
+        <div className="icons right">
+            <i className="material-icons">videocam</i>
+            <i className="material-icons">apps</i>
+            <i className="material-icons">notifications</i>
+            <i className="material-icons display-this">account_circle</i>
+        </div>
+    </header>
   )
 }
 
